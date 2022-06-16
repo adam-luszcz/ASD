ca8def54333aa973481c8be3fbcecf10a9ed54b5
adam-luszcz <adam.luszcz20@gmail.com>
2022-06-16 23:05:53 +0200

    probably final commit

--- projekt2/app.py
+++ projekt2/app.py
@@ -1,4 +1,4 @@
-from projekt2.heap import *
+from heap import heap_deq, heap_enq, is_min_heap
 
 
 def build_occurrence_dict(text: str) -> dict:
@@ -18,13 +18,44 @@ def build_priority_queue(occurrence_dict: dict) -> list:
     return queue
 
 
+def build_tree(queue: list):
+    while len(queue) > 1:
+        a = heap_deq(queue)
+        b = heap_deq(queue)
+        heap_enq(queue, (a[0] + b[0], '', a, b))
+    return heap_deq(queue)
+
+
+def encode(n, binary_rep, txt):
+    if n[1] != '':
+        with open('output.txt', 'a') as f:
+            f.write(f'{n[1]}: {binary_rep}\n')
+        print(f'{n[1]}: {binary_rep}')
+        txt = txt.replace(n[1], binary_rep)
+        return txt
+    txt = encode(n[2], binary_rep + '1', txt)
+    txt = encode(n[3], binary_rep + '0', txt)
+    return txt
+
+
 def main():
     with open('input_text.txt', 'r') as f:
         text = f.read()
 
-    # print(build_occurrence_dict(text))
     occurrence_dict = build_occurrence_dict(text)
-    print(build_priority_queue(occurrence_dict))
+    queue = build_priority_queue(occurrence_dict)
+    # print(queue)
+    # print(is_min_heap([n[0] for n in queue]))
+    # print(occurrence_dict)
+    print('\nTablica kodowania: ')
+    root_node = build_tree(queue)
+
+    result = encode(root_node, '', text)
+    print(f'\nZakodowany tekst: {result}')
+
+    with open('output.txt', 'a', encoding='utf-8') as f:
+        for i in range(0, len(result), 8):
+            f.write(chr(int(result[i:8+i], 2)))
 
 
 if __name__ == '__main__':
--- projekt2/heap.py
+++ projekt2/heap.py
@@ -1,14 +1,14 @@
 global n
 
 
-def min_heapify(A: list, i: int):
+def min_heapify(A: list, i):
     l = (2 * i) + 1
     r = (2 * i) + 2
-    if l < n and A[l] < A[i]:
+    if l < len(A) and A[l] < A[i]:
         smallest = l
     else:
         smallest = i
-    if r < n and A[r] < A[smallest]:
+    if r < len(A) and A[r] < A[smallest]:
         smallest = r
     if smallest != i:
         A[i], A[smallest] = A[smallest], A[i]
@@ -18,7 +18,7 @@ def min_heapify(A: list, i: int):
 def build_min_heap(A: list):
     global n
     n = len(A) - 1
-    for i in range((n // 2) - 1, -1, -1):
+    for i in range((n // 2), -1, -1):
         min_heapify(A, i)
 
 
@@ -28,9 +28,22 @@ def heap_enq(A: list, i):
 
 
 def heap_deq(A: list):
+    global n
     if n < 1:
         return A.pop()
     smallest, A[0] = A[0], A.pop()
     n -= 1
     min_heapify(A, 0)
     return smallest
+
+
+# for debugging
+def is_min_heap(A: list):
+    for i in range(0, (len(A) - 1) // 2):
+        print(f'{A[i]}, {A[(2 * i) + 1]}, {A[(2 * i) + 2]}')
+        if A[i] > A[(2 * i) + 1] or A[i] > A[(2 * i) + 2]:
+            return False
+    print(f'{A[(len(A) // 2) - 1]}, {A[-1]}')
+    if A[(len(A) // 2) - 1] > A[-1]:
+        return False
+    return True
